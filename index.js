9e21cfae3116e7250d7e2cb3b48650626a5a0259
Dino <gamer.n.coder@gmail.com>
2018-07-24 18:43:43 +0200

    replace ejs with hbs. Add layouting

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.set('view engine', 'ejs');
+app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname, 'assets')));
+app.set('view options', { layout: 'layout.hbs' });
 
-app.get('/', (req, res) => {
-  res.render('index', { title: 'Hey', message: 'Hello there!' });
+
+app.get('/matija', (req, res) => {
+  res.render('matija', { name: 'Matija' });
 });
 
 app.listen(3000, () => {
