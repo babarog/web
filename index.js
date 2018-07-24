58ad77db785cfbf00855a722823f69cfbb5fb04a
Dino <dinokopriva@hotmail.com>
2018-07-24 19:24:29 +0200

    add nav

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('hbs');
+
+const routes = require('./routes');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'assets')));
-app.set('view options', { layout: 'layout.hbs' });
+app.set('view options', { layout: 'layout' });
+hbs.registerPartials(path.join(__dirname, '/views/partials'));
 
+const globalVars = { routes };
 
-app.get('/matija', (req, res) => {
-  res.render('matija', { name: 'Matija' });
+app.get(routes.home, (req, res) => {
+  res.render('home', globalVars);
+});
+
+app.get(routes.matija, (req, res) => {
+  res.render('matija', { ...globalVars, name: 'Matija' });
 });
 
 app.listen(3000, () => {
